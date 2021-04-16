d97bdc5fbe82d30c659721560655f7660758ffd3
Albert Banshchikov <cozyblackmold@gmail.com>
2021-04-16 12:48:38 +0700

    add method to ClientRepo

--- authorization-server/src/main/java/edu/albert/studycards/authserver/repository/ClientRepository.java
+++ authorization-server/src/main/java/edu/albert/studycards/authserver/repository/ClientRepository.java
@@ -1,7 +1,13 @@
 package edu.albert.studycards.authserver.repository;
 
+import edu.albert.studycards.authserver.domain.interfaces.ClientPersistent;
 import edu.albert.studycards.authserver.domain.persistent.ClientPersistentImpl;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<ClientPersistentImpl, Long> {
+	
+	Optional<ClientPersistent> findByEmail(String email);
+	
 }
