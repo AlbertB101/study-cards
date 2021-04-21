0ee72d218f6d5218e8e261412053c20bf9dec3aa
Albert Banshchikov <cozyblackmold@gmail.com>
2021-04-21 15:53:51 +0700

    add implementation to update client query

--- authorization-server/src/main/java/edu/albert/studycards/authserver/repository/ClientRepository.java
+++ authorization-server/src/main/java/edu/albert/studycards/authserver/repository/ClientRepository.java
@@ -3,11 +3,24 @@ package edu.albert.studycards.authserver.repository;
 import edu.albert.studycards.authserver.domain.interfaces.ClientPersistent;
 import edu.albert.studycards.authserver.domain.persistent.ClientPersistentImpl;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<ClientPersistentImpl, Long> {
 	
 	Optional<ClientPersistent> findByEmail(String email);
+	
 	boolean existsByEmail(String email);
+	
+	//	@Transactional
+	@Modifying(clearAutomatically = true)
+	@Query("update Client c set c.firstName = :firstName, c.lastName = :lastName where c.email = :email")
+	void updateClientByEmail(@Param("email") String email,
+	                         @Param("firstName") String firsName,
+	                         @Param("lastName") String lastName);
 }
