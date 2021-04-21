4b0008256f9b26e813a502c4d6dc5ef27d18caef
Albert Banshchikov <cozyblackmold@gmail.com>
2021-04-21 12:22:48 +0700

    add configuration class

---
+++ authorization-server/src/main/java/edu/albert/studycards/authserver/config/AppConfig.java
@@ -0,0 +1,28 @@
+package edu.albert.studycards.authserver.config;
+
+import org.springframework.context.annotation.AdviceMode;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@EnableScheduling
+@EnableAsync(mode = AdviceMode.ASPECTJ) // Changes here!!!
+@Configuration
+public class AppConfig  {
+	
+	@Bean
+	public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(6);
+		executor.setMaxPoolSize(12);
+		executor.setQueueCapacity(150);
+		executor.setThreadGroupName("SomeThreadPoolXD");
+		executor.initialize();
+		return executor;
+	}
+}
