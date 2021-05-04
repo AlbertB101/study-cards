112a0eb69a601ea400106e0ee7372cef43378280
Albert Banshchikov <cozyblackmold@gmail.com>
2021-05-04 11:16:34 +0700

    add changes to UserDetailsMethod

--- authorization-server/src/main/java/edu/albert/studycards/authserver/security/UserDetailsServiceImpl.java
+++ authorization-server/src/main/java/edu/albert/studycards/authserver/security/UserDetailsServiceImpl.java
@@ -1,6 +1,8 @@
 package edu.albert.studycards.authserver.security;
 
+import edu.albert.studycards.authserver.domain.interfaces.AccountPersistent;
 import edu.albert.studycards.authserver.domain.interfaces.ClientPersistent;
+import edu.albert.studycards.authserver.repository.AccountRepository;
 import edu.albert.studycards.authserver.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,13 +14,18 @@ import org.springframework.stereotype.Service;
 public class UserDetailsServiceImpl implements UserDetailsService {
 	
 	@Autowired
-	private ClientRepository accountRepository;
+	private ClientRepository clientRepo;
+	@Autowired
+	private AccountRepository accountRepo;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		ClientPersistent account = accountRepository.findByEmail(username)
-			                           .orElseThrow(() -> new UsernameNotFoundException("User doesn't exists"));
-		return SecurityAccount.fromUser(account);
+		ClientPersistent client = clientRepo.findByEmail(username)
+			                          .orElseThrow(() -> new UsernameNotFoundException("Client doesn't exists"));
+		AccountPersistent account = accountRepo.findByClient_Email(username)
+			                            .orElseThrow(() -> new UsernameNotFoundException("Account doesn't exists"));
+		
+		return SecurityAccount.fromUser(client, account);
 	}
 	
 	
