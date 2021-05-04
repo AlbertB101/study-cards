c1e249a314653963086d7dfefd979d39178ed29f
Albert Banshchikov <cozyblackmold@gmail.com>
2021-05-04 11:11:21 +0700

    enableAsync without parameters

--- authorization-server/src/main/java/edu/albert/studycards/authserver/config/AppConfig.java
+++ authorization-server/src/main/java/edu/albert/studycards/authserver/config/AppConfig.java
@@ -1,9 +1,7 @@
 package edu.albert.studycards.authserver.config;
 
-import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -11,7 +9,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 @EnableScheduling
-@EnableAsync(mode = AdviceMode.ASPECTJ) // Changes here!!!
+@EnableAsync
 @Configuration
 public class AppConfig  {
 	
