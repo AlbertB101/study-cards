ea3a1f140955471223dac4467db09edb6c1a5986
Albert Banshchikov <cozyblackmold@gmail.com>
2021-05-04 11:15:41 +0700

    change fromUser method

--- authorization-server/src/main/java/edu/albert/studycards/authserver/security/SecurityAccount.java
+++ authorization-server/src/main/java/edu/albert/studycards/authserver/security/SecurityAccount.java
@@ -1,5 +1,6 @@
 package edu.albert.studycards.authserver.security;
 
+import edu.albert.studycards.authserver.domain.interfaces.AccountPersistent;
 import edu.albert.studycards.authserver.domain.interfaces.ClientPersistent;
 import edu.albert.studycards.authserver.domain.interfaces.Status;
 import org.springframework.security.core.GrantedAuthority;
@@ -10,66 +11,66 @@ import java.util.Collection;
 import java.util.List;
 
 public class SecurityAccount implements UserDetails {
-    
-    private final String username;
-    private final String password;
-    private final List<SimpleGrantedAuthority> authorities;
-    private final boolean isActive;
-    
-    public SecurityAccount(String username,
-                           String password,
-                           List<SimpleGrantedAuthority> authorities,
-                           boolean isActive) {
-        this.username = username;
-        this.password = password;
-        this.authorities = authorities;
-        this.isActive = isActive;
-    }
-    
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
-    
-    @Override
-    public String getPassword() {
-        return password;
-    }
-    
-    @Override
-    public String getUsername() {
-        return username;
-    }
-    
-    @Override
-    public boolean isAccountNonExpired() {
-        return isActive;
-    }
-    
-    @Override
-    public boolean isAccountNonLocked() {
-        return isActive;
-    }
-    
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return isActive;
-    }
-    
-    @Override
-    public boolean isEnabled() {
-        return isActive;
-    }
-    
-    public static UserDetails fromUser(ClientPersistent client) {
-        return new org.springframework.security.core.userdetails.User(
-                client.getEmail(),
-                client.getPassword(),
-                client.getStatus().equals(Status.ACTIVE),
-                client.getStatus().equals(Status.ACTIVE),
-                client.getStatus().equals(Status.ACTIVE),
-                client.getStatus().equals(Status.ACTIVE),
-                client.getRole().getAuthorities()
-        );
-    }
+	
+	private final String username;
+	private final String password;
+	private final List<SimpleGrantedAuthority> authorities;
+	private final boolean isActive;
+	
+	public SecurityAccount(String username,
+	                       String password,
+	                       List<SimpleGrantedAuthority> authorities,
+	                       boolean isActive) {
+		this.username = username;
+		this.password = password;
+		this.authorities = authorities;
+		this.isActive = isActive;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+	
+	@Override
+	public String getPassword() {
+		return password;
+	}
+	
+	@Override
+	public String getUsername() {
+		return username;
+	}
+	
+	@Override
+	public boolean isAccountNonExpired() {
+		return isActive;
+	}
+	
+	@Override
+	public boolean isAccountNonLocked() {
+		return isActive;
+	}
+	
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return isActive;
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		return isActive;
+	}
+	
+	public static UserDetails fromUser(ClientPersistent client, AccountPersistent account) {
+		return new org.springframework.security.core.userdetails.User(
+			client.getEmail(),
+			client.getPassword(),
+			account.getStatus().equals(Status.ACTIVE),
+			account.getStatus().equals(Status.ACTIVE),
+			account.getStatus().equals(Status.ACTIVE),
+			account.getStatus().equals(Status.ACTIVE),
+			account.getRole().getAuthorities()
+		);
+	}
 }
