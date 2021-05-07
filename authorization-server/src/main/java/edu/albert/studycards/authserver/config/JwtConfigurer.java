53ab248a828148aec8d635d0833661bb7cca7952
Albert Banshchikov <cozyblackmold@gmail.com>
2021-05-07 15:44:19 +0700

    delete old filter from configuration

--- authorization-server/src/main/java/edu/albert/studycards/authserver/config/JwtConfigurer.java
+++ authorization-server/src/main/java/edu/albert/studycards/authserver/config/JwtConfigurer.java
@@ -1,7 +1,6 @@
 package edu.albert.studycards.authserver.config;
 
 import edu.albert.studycards.authserver.security.filter.JwtAuthorizationFilter;
-import edu.albert.studycards.authserver.security.filter.JwtAuthorizationValidationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,13 +12,10 @@ import org.springframework.stereotype.Component;
 public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 	@Autowired
 	private JwtAuthorizationFilter jwtAuthorizationFilter;
-	@Autowired
-	private JwtAuthorizationValidationFilter jwtAuthorizationValidationFilter;
 	
 	@Override
 	public void configure(HttpSecurity httpSecurity) {
 		httpSecurity.addFilterAfter(jwtAuthorizationFilter, BasicAuthenticationFilter.class);
-		httpSecurity.addFilterBefore(jwtAuthorizationValidationFilter, JwtAuthorizationFilter.class);
 	}
 	
 }
