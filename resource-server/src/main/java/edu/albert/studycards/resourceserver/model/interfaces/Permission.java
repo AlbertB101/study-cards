853a0759151306665ba18fdc4f8e1e86a25dbde1
Albert Banshchikov <cozyblackmold@gmail.com>
2021-06-22 19:45:19 +0700

    add permissions, roles and statuses

---
+++ resource-server/src/main/java/edu/albert/studycards/resourceserver/model/interfaces/Permission.java
@@ -0,0 +1,22 @@
+package edu.albert.studycards.resourceserver.model.interfaces;
+
+public enum Permission {
+    USER_WRITE("user:write"),
+    USER_READ("user:read"),
+    USER_UPDATE("user:update"),
+    USER_DELETE("user:delete"),
+    DEVELOPER_READ("developer:read"),
+    DEVELOPER_WRITE("developer:write"),
+    DEVELOPER_UPDATE("developer:update"),
+    DEVELOPER_DELETE("developer:delete");
+    
+    private final String permission;
+    
+    Permission(String permission) {
+        this.permission = permission;
+    }
+    
+    public String getPermission() {
+        return permission;
+    }
+}
---
+++ resource-server/src/main/java/edu/albert/studycards/resourceserver/model/interfaces/Status.java
@@ -0,0 +1,5 @@
+package edu.albert.studycards.resourceserver.model.interfaces;
+
+public enum Status {
+    ACTIVE, BANNED
+}
