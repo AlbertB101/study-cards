2a9cedca4f144837a60d302c4f620bd12bd5371b
Albert Banshchikov <cozyblackmold@gmail.com>
2021-06-23 14:13:35 +0700

    add basic methods to LangPack interface

--- resource-server/src/main/java/edu/albert/studycards/resourceserver/model/interfaces/LangPack.java
+++ resource-server/src/main/java/edu/albert/studycards/resourceserver/model/interfaces/LangPack.java
@@ -5,14 +5,13 @@ import edu.albert.studycards.resourceserver.model.dto.LangPackDtoImpl;
 import java.util.List;
 
 public interface LangPack {
-	static LangPackDto persistentToDto(LangPackPersistent langPackPersistent) {
-		LangPackDto langPackDto = new LangPackDtoImpl();
-		langPackDto.setId(langPackPersistent.getId());
-		langPackDto.setLang(langPackPersistent.getLang());
-		langPackDto.setAccountId(langPackPersistent.getAccount().getId());
-		List<CardDto> dtoList = Card.from(langPackPersistent.getCards());
-		langPackDto.setCards(dtoList);
-		
-		return langPackDto;
-	}
+	
+	Long getId();
+	void setId(Long id);
+	
+	Long getAccountId();
+	void setAccountId(Long accountId);
+	
+	String getLang();
+	void setLang(String lang);
 }
