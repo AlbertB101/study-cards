962473eb573ebe8a3b5a1a20ccb3c68b08d34285
Albert Banshchikov <cozyblackmold@gmail.com>
2021-06-23 23:04:55 +0700

    make changes to methods

--- resource-server/src/main/java/edu/albert/studycards/resourceserver/service/CardService.java
+++ resource-server/src/main/java/edu/albert/studycards/resourceserver/service/CardService.java
@@ -2,12 +2,11 @@ package edu.albert.studycards.resourceserver.service;
 
 import edu.albert.studycards.resourceserver.model.dto.CardDtoImpl;
 import edu.albert.studycards.resourceserver.model.interfaces.*;
-import edu.albert.studycards.resourceserver.model.persistent.*;
-import edu.albert.studycards.resourceserver.repository.*;
-
+import edu.albert.studycards.resourceserver.model.persistent.CardPersistentImpl;
+import edu.albert.studycards.resourceserver.model.persistent.LangPackPersistentImpl;
+import edu.albert.studycards.resourceserver.repository.CardRepository;
+import edu.albert.studycards.resourceserver.repository.LangPackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
@@ -16,23 +15,19 @@ import java.util.NoSuchElementException;
 public class CardService {
 	
 	@Autowired
-	CardRepository cardRepository;
+	CardRepository cardRepo;
 	@Autowired
-	LangPackRepository langPackRepository;
+	LangPackRepository langPackRepo;
 	@Autowired
 	LangPackService langPackService;
 	
 	public void create(CardDto cardDto) {
-		try {
-			var langPackFromRepository = langPackService.find(
-				cardDto.getAccountId(),
-				cardDto.getLang());
-			CardPersistentImpl card = new CardPersistentImpl(cardDto, langPackFromRepository);
-			
-			cardRepository.saveAndFlush(card);
-		} catch (RuntimeException e) {
-			throw new RuntimeException(e.getMessage());
+		LangPackPersistent langPack = langPackService.find(cardDto.getLang());
+		CardPersistent card = null;
+		if (!langPack.hasCard(cardDto.getWord())) {
+			card = new CardPersistentImpl(cardDto, langPack);
 		}
+		cardRepo.saveAndFlush((CardPersistentImpl) card);
 	}
 	
 	public CardDto get(String word, String lang) throws NoSuchElementException {
@@ -40,33 +35,18 @@ public class CardService {
 		return new CardDtoImpl(cardPersistent);
 	}
 	
-	public CardDto get(String word, String lang, long accountId) throws NoSuchElementException {
-		CardPersistent cardPersistent = findCard(word, lang, accountId);
-		return new CardDtoImpl(cardPersistent);
-	}
-	
-	@Deprecated
 	public void update(CardDto cardDto) throws RuntimeException {
 		CardPersistent card = findCard(cardDto.getWord(), cardDto.getLang());
-		
 		card.setWordTr(cardDto.getWordTr());
 		card.setWordMng(cardDto.getWordMng());
 		//TODO: add update query to card repo
-//		save(card);
+		cardRepo.saveAndFlush((CardPersistentImpl) card);
 	}
 	
 	public void delete(String word, String lang) throws NoSuchElementException {
 		LangPackPersistent langPack = langPackService.find(lang);
 		langPack.deleteCard(word);
-		langPackRepository.saveAndFlush((LangPackPersistentImpl) langPack);
-	}
-	
-	public ResponseEntity<?> delete(String word, String lang, long accountId) throws NoSuchElementException {
-		LangPackPersistent langPack = langPackService.find(accountId, lang);
-		langPack.deleteCard(word);
-		langPackRepository.saveAndFlush((LangPackPersistentImpl) langPack);
-		
-		return new ResponseEntity<>(true, HttpStatus.OK);
+		langPackRepo.saveAndFlush((LangPackPersistentImpl) langPack);
 	}
 	
 	private CardPersistent findCard(String word, String lang) throws NoSuchElementException {
@@ -74,15 +54,8 @@ public class CardService {
 		return langPack.getCard(word);
 	}
 	
-	private CardPersistent findCard(String word, String lang, long accountId) {
-		CardPersistent card = null;
+	private CardPersistent findCard(String word, String lang, long accountId) throws NoSuchElementException {
 		LangPackPersistent langPack = langPackService.find(accountId, lang);
-		try {
-			card = langPack.getCard(word);
-		} catch (NoSuchElementException e) {
-			//TODO: add logging
-			System.err.println(e.getClass());
-		}
-		return card;
+		return langPack.getCard(word);
 	}
 }
