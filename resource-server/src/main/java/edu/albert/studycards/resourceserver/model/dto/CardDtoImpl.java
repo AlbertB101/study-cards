15dcf09261997c831bf6bf32f89f09d409cc1e9c
Albert Banshchikov <cozyblackmold@gmail.com>
2021-06-23 14:21:41 +0700

    add card dto implementation

---
+++ resource-server/src/main/java/edu/albert/studycards/resourceserver/model/dto/CardDtoImpl.java
@@ -0,0 +1,47 @@
+package edu.albert.studycards.resourceserver.model.dto;
+
+import edu.albert.studycards.resourceserver.model.interfaces.CardDto;
+import edu.albert.studycards.resourceserver.model.interfaces.CardPersistent;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class CardDtoImpl implements CardDto {
+    private Long id;
+    private Long accountId;
+    private String lang;
+    private String word;
+    private String wordTr;
+    private String wordMng;
+    
+    public CardDtoImpl(String word, String wordTr, String wordMng) {
+        this.word = word;
+        this.wordTr = wordTr;
+        this.wordMng = wordMng;
+    }
+    
+    public CardDtoImpl(CardDto card) {
+        this.id = card.getId();
+        this.accountId = card.getAccountId();
+        this.lang = card.getLang();
+        this.word = card.getWord();
+        this.wordTr = card.getWordTr();
+        this.wordMng = card.getWordMng();
+    }
+    
+    /**
+     * <p>Constructor for creating new Dto entity using {@link CardPersistent}</p>
+     *
+     * @param card {@link CardPersistent} implementation.
+     */
+    public CardDtoImpl(CardPersistent card) {
+        this.id = card.getId();
+        this.accountId = card.getLangPack().getId();
+        this.word = card.getWord();
+        this.wordTr = card.getWordTr();
+        this.wordMng = card.getWordMng();
+    }
+}
