6212caaff2011f8c7dc4cf17dd627425916e6c39
Albert Banshchikov <cozyblackmold@gmail.com>
2021-06-23 14:47:57 +0700

    add initial version of LangPackDtoImpl

---
+++ resource-server/src/main/java/edu/albert/studycards/resourceserver/model/dto/LangPackDtoImpl.java
@@ -0,0 +1,100 @@
+package edu.albert.studycards.resourceserver.model.dto;
+
+import edu.albert.studycards.resourceserver.model.interfaces.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LangPackDtoImpl implements LangPackDto {
+	private Long id;
+	private Long accountId;
+	private String lang;
+	private List<CardDto> cards = new ArrayList<>();
+	
+	public LangPackDtoImpl(String lang) {
+		this.lang = lang;
+	}
+	
+	public LangPackDtoImpl(LangPackDto langPackDto) {
+		this.id = langPackDto.getId();
+		this.accountId = langPackDto.getAccountId();
+		this.lang = langPackDto.getLang();
+		this.cards = langPackDto.getCards();
+	}
+	
+	@Override
+	public void addCard(CardDto card) {
+		Objects.requireNonNull(card);
+		cards.add(card);
+	}
+	
+	@Override
+	public void setCard(CardDto givenCard) throws NoSuchElementException {
+		CardDto card = getCard(givenCard.getWord());
+		cards.set(getIndexOf(card), givenCard);
+	}
+	
+	@Override
+	public void deleteCard(int n) {
+		if (n >= 0 && n < cards.size())
+			cards.remove(n);
+	}
+	
+	@Override
+	public void deleteCard(String word) {
+		if (exists(word)) {
+			CardDto card = getCard(word);
+			int cardIndex = getIndexOf(card);
+			cards.remove(cardIndex);
+		}
+	}
+	
+	@Override
+	public void clearCards() {
+		cards.clear();
+	}
+	
+	@Override
+	public CardDto getCard(int n) {
+		return cards.get(n);
+	}
+	
+	@Override
+	public CardDto getCard(String word) throws NoSuchElementException {
+		return cards.stream()
+			       .findFirst()
+			       .filter(card -> card.getWord().equals(word))
+			       .orElseThrow(NoSuchElementException::new);
+	}
+	
+	@Override
+	public CardDto getCard(Long id) {
+		return cards.stream().findFirst()
+			       .filter(card -> card.getId().equals(id))
+			       .get();
+	}
+	
+	@Override
+	public int getIndexOf(CardDto card) {
+		return cards.indexOf(card);
+	}
+	
+	@Override
+	public boolean exists(String word) {
+		return cards.stream()
+			       .anyMatch(card -> card.getWord().equals(word));
+	}
+	
+	@Override
+	public int size() {
+		return cards.size();
+	}
+}
