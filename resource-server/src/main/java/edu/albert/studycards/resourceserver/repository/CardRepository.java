be9c71b7aeb0bc6ee3766432739e5edb2de2fcb3
Albert Banshchikov <cozyblackmold@gmail.com>
2021-06-26 00:02:44 +0700

    add find method

--- resource-server/src/main/java/edu/albert/studycards/resourceserver/repository/CardRepository.java
+++ resource-server/src/main/java/edu/albert/studycards/resourceserver/repository/CardRepository.java
@@ -1,7 +1,11 @@
 package edu.albert.studycards.resourceserver.repository;
 
+import edu.albert.studycards.resourceserver.model.interfaces.CardPersistent;
 import edu.albert.studycards.resourceserver.model.persistent.CardPersistentImpl;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CardRepository extends JpaRepository<CardPersistentImpl, Long> {
+	Optional<CardPersistent> findByWordAndLangPack_AccountEmail(String word, String accEmail);
 }
