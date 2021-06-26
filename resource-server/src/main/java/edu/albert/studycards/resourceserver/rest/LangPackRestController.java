409d841294906e14ba449c6e03069535882eaabf
Albert Banshchikov <cozyblackmold@gmail.com>
2021-06-26 14:46:22 +0700

    add minor improvements to lang pack controller

--- resource-server/src/main/java/edu/albert/studycards/resourceserver/rest/LangPackRestController.java
+++ resource-server/src/main/java/edu/albert/studycards/resourceserver/rest/LangPackRestController.java
@@ -2,23 +2,24 @@ package edu.albert.studycards.resourceserver.rest;
 
 import edu.albert.studycards.resourceserver.exceptions.LangPackAlreadyExistsException;
 import edu.albert.studycards.resourceserver.model.dto.LangPackDtoImpl;
+import edu.albert.studycards.resourceserver.model.interfaces.LangPackDto;
 import edu.albert.studycards.resourceserver.service.LangPackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.NoSuchElementException;
+import java.util.Map;
 
-//TODO: Make RespEntities return more precise info
 @RestController
 @RequestMapping("/api/v1/langPack")
 public class LangPackRestController {
+    
+    private static final String RESPONSE_MSG_FOR_RUNTIME_EXC =
+    "Input values are incorrect or internal server error";
     
     @Autowired
     private LangPackService langPackService;
@@ -27,20 +28,33 @@ public class LangPackRestController {
     @PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> createLangPack(@RequestBody @Valid LangPackDtoImpl langPack) {
         try {
-            langPackService.create(langPack);
-        } catch (RuntimeException | LangPackAlreadyExistsException e) {
+            LangPackDto createdLangPack = langPackService.create(langPack);
+            Map<String, Object> response = Map.of(
+                "ResponseMessage", "LangPack was successfully created",
+                "Card", createdLangPack);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (LangPackAlreadyExistsException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(
+                "LangPack wasn't created." + RESPONSE_MSG_FOR_RUNTIME_EXC,
+                HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(HttpStatus.OK);
     }
     
     @PreAuthorize("hasAuthority('user:read')")
     @GetMapping(value = "/get", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getLangPack(@RequestParam(name = "lang") String lang) {
         try {
-            return new ResponseEntity<>(langPackService.get(lang), HttpStatus.OK);
-        } catch (NoSuchElementException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+            LangPackDto receivedLangPack = langPackService.get(lang);
+            Map<String, Object> response = Map.of(
+                "ResponseMessage", "LangPack was successfully created",
+                "Card", receivedLangPack);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(
+                "LangPack wasn't created." + RESPONSE_MSG_FOR_RUNTIME_EXC,
+                HttpStatus.BAD_REQUEST);
         }
     }
     
@@ -59,22 +73,29 @@ public class LangPackRestController {
     @PreAuthorize("hasAuthority('user:update')")
     public ResponseEntity<?> updateLangPack(@RequestBody @Valid LangPackDtoImpl langPackDtoImpl) {
         try {
-            langPackService.update(langPackDtoImpl);
-        } catch (NoSuchElementException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+            LangPackDto updatedLangPack = langPackService.update(langPackDtoImpl);
+            Map<String, Object> response = Map.of(
+                "ResponseMessage", "LangPack was successfully created",
+                "Card", updatedLangPack);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(
+                "LangPack wasn't updated." + RESPONSE_MSG_FOR_RUNTIME_EXC,
+                HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(HttpStatus.OK);
     }
     
-    @PostMapping(value = "/delete")
+    @PostMapping(value = "/delete/{id}")
     @PreAuthorize("hasAuthority('user:delete')")
-    public ResponseEntity<?> deleteLangPack(@RequestParam("lang") String lang) {
+    public ResponseEntity<?> deleteLangPack(@PathVariable Long id) {
         try {
-//            langPackService.delete();
-        } catch (NoSuchElementException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+            langPackService.delete(id);
+            return new ResponseEntity<>("LangPack was successfully deleted", HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(
+                "LangPack wasn't updated." + RESPONSE_MSG_FOR_RUNTIME_EXC,
+                HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(HttpStatus.OK);
     }
     
 //    @PreAuthorize("hasAuthority('developer:read')")
