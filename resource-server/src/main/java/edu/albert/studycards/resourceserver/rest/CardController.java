0833305fbd1a4cfffdd3f59c113563542e4fa7a7
Albert Banshchikov <cozyblackmold@gmail.com>
2021-06-26 14:11:02 +0700

    improve card controller return info

--- resource-server/src/main/java/edu/albert/studycards/resourceserver/rest/CardController.java
+++ resource-server/src/main/java/edu/albert/studycards/resourceserver/rest/CardController.java
@@ -11,59 +11,87 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Map;
 
-//TODO: Make RespEntities return more precise info
 @RestController
 @RequestMapping("api/v1/card")
 public class CardController {
+	
+	@Autowired
+	CardService cardService;
+	
+	@PostMapping(value = "/create")
+	@PreAuthorize("hasAuthority('user:write')")
+	public ResponseEntity<?> create(@RequestBody @Valid CardDtoImpl cardDto) {
+		try {
+			CardDto createdCard = cardService.create(cardDto);
+			Map<String, Object> response = Map.of(
+				"ResponseMessage", "Card was successfully created",
+				"Card", createdCard);
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		} catch (RuntimeException e) {
+			return new ResponseEntity<>(
+				"Card wasn't created. Incorrect input value or internal server error",
+				HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@GetMapping(value = "/get", produces = MediaType.APPLICATION_JSON_VALUE)
+	@PreAuthorize("hasAuthority('user:read')")
+	public ResponseEntity<?> get(@RequestParam(name = "word") String word) {
+		try {
+			CardDto receivedCard = cardService.get(word);
+			Map<String, Object> response = Map.of(
+				"ResponseMessage", "Card was successfully received",
+				"Card", receivedCard);
+			return new ResponseEntity<>(receivedCard, HttpStatus.OK);
+		} catch (RuntimeException e) {
+			return new ResponseEntity<>(
+				"Card wasn't found. Incorrect input value or internal server error",
+				HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@PostMapping(value = "/update", consumes = MediaType.APPLICATION_JSON_VALUE)
+	@PreAuthorize("hasAuthority('user:update')")
+	public ResponseEntity<?> update(@RequestBody @Valid CardDtoImpl cardDto) {
+		try {
+			CardDto updatedCard = cardService.update(cardDto);
+			Map<String, Object> response = Map.of(
+				"ResponseMessage", "Card was successfully received",
+				"Card", updatedCard);
+			return new ResponseEntity<>(updatedCard, HttpStatus.OK);
+		} catch (RuntimeException e) {
+			return new ResponseEntity<>(
+				"Card wasn't updated. Incorrect input value or internal server error",
+				HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@PostMapping(value = "/delete")
+	@PreAuthorize("hasAuthority('user:delete')")
+	public ResponseEntity<?> delete(@RequestParam("word") String word,
+	                                @RequestParam("lang") String lang) {
+		try {
+			cardService.delete(word, lang);
+			return new ResponseEntity<>("Card was successfully deleted", HttpStatus.OK);
+		} catch (RuntimeException e) {
+			return new ResponseEntity<>(
+				"Card wasn't deleted. Incorrect input value or internal server error",
+				HttpStatus.BAD_REQUEST);
+		}
+	}
     
-    @Autowired
-    CardService cardService;
-    
-    @PostMapping(value = "/create")
-    @PreAuthorize("hasAuthority('user:write')")
-    public ResponseEntity<?> create(@RequestBody @Valid CardDtoImpl cardDto) {
-        try {
-            cardService.create(cardDto);
-            return new ResponseEntity<>("Card was successfully created", HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>("Card wasn't created", HttpStatus.NOT_FOUND);
-        }
-    }
-    
-    @GetMapping(value = "/get", produces = MediaType.APPLICATION_JSON_VALUE)
-    @PreAuthorize("hasAuthority('user:read')")
-    public ResponseEntity<?> get(@RequestParam(name = "word") String word,
-                                 @RequestParam(name = "lang") String lang) {
-        try {
-            CardDto cardDto = cardService.get(word, lang);
-            return new ResponseEntity<>(cardDto, HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>("Card wasn't updated", HttpStatus.NOT_FOUND);
-        }
-    }
-    
-    @PostMapping(value = "/update", consumes = MediaType.APPLICATION_JSON_VALUE)
-    @PreAuthorize("hasAuthority('user:update')")
-    public ResponseEntity<?> update(@RequestBody @Valid CardDtoImpl cardDto) {
-        try {
-            cardService.update(cardDto);
-            return new ResponseEntity<>("Card was successfully created", HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>("Card wasn't updated", HttpStatus.NOT_FOUND);
-        }
-    }
-    
-    @PostMapping(value = "/delete")
+    @PostMapping(value = "/delete/{id}")
     @PreAuthorize("hasAuthority('user:delete')")
-    public ResponseEntity<?> delete(@RequestParam("word") String word,
-                                    @RequestParam(name = "lang") String lang) {
+    public ResponseEntity<?> delete(@PathVariable Long id) {
         try {
-            cardService.delete(word, lang);
+            cardService.delete(id);
             return new ResponseEntity<>("Card was successfully deleted", HttpStatus.OK);
         } catch (RuntimeException e) {
-            return new ResponseEntity<>("Card wasn't deleted", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(
+                "Card wasn't deleted. Incorrect input value or internal server error",
+                HttpStatus.BAD_REQUEST);
         }
     }
-    
 }
