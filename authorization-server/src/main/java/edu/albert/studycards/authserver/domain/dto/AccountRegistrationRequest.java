e0ce9afbe86ec2b85ff6a236f30b05315ae2cb32
Albert Banshchikov <cozyblackmold@gmail.com>
2021-06-27 15:41:11 +0700

    update interface

--- authorization-server/src/main/java/edu/albert/studycards/authserver/domain/dto/AccountRegistrationRequest.java
+++ authorization-server/src/main/java/edu/albert/studycards/authserver/domain/dto/AccountRegistrationRequest.java
@@ -1,4 +1,19 @@
 package edu.albert.studycards.authserver.domain.dto;
 
 public interface AccountRegistrationRequest {
+	String getFirstName();
+	
+	String getLastName();
+	
+	String getEmail();
+	
+	String getPassword();
+	
+	void setFirstName(String firstName);
+	
+	void setLastName(String lastName);
+	
+	void setEmail(String email);
+	
+	void setPassword(String password);
 }
